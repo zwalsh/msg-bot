3ab42d7cc2d4f117be884f9dd129a3918b95007f
Zach Walsh <zach.c.walsh@gmail.com>
2021-03-22 17:49:17 -0400

    Slow it down

--- read_message.py
+++ read_message.py
@@ -4,16 +4,17 @@ import time
 logger = logging.getLogger()
 
 # length of time in seconds for a morse Dot
+# Standard is:
 # dot = 1 unit
 # dash = 3 units
 # between letters = 3 units
 # between words = 7 units
-MORSE_TIME_UNIT_S = 1.0 / 3.0
+MORSE_TIME_UNIT_S = 2.0 / 3.0
 DOT = MORSE_TIME_UNIT_S
 DASH = MORSE_TIME_UNIT_S * 3
-SYMBOL_PAUSE = MORSE_TIME_UNIT_S
-LETTER_PAUSE = MORSE_TIME_UNIT_S * 3
-WORD_PAUSE = MORSE_TIME_UNIT_S * 7
+SYMBOL_PAUSE = MORSE_TIME_UNIT_S * 1.5
+LETTER_PAUSE = MORSE_TIME_UNIT_S * 6
+WORD_PAUSE = MORSE_TIME_UNIT_S * 10
 
 MORSE_CODE_DICT = {
     'A': '.-',
