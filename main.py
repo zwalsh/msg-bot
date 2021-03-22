7644354115401c0049057671622004c08a74bf16
Zach Walsh <zach.c.walsh@gmail.com>
2021-03-22 16:27:24 -0400

    Check for read/unread on file system

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import os
+from pathlib import Path
 
 from control_loop import run_loop
 from hardware.Button import Button
@@ -9,6 +11,13 @@ from read_message import read_message
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s [%(levelname)s] (%(threadName)-10s) %(message)s', )
 
+logger = logging.getLogger()
+
 if __name__ == '__main__':
-    read_message("Hi Jaclyn", Light())
+    data_path = os.getenv("MSG_BOT_DATA_PATH")
+    logger.info(f"Starting up with data path {data_path}")
+    message = Message(Path(data_path or "data"))
+    logging.getLogger().info(f"Is message unread? {message.is_unread()}")
+    read_message(message.get_message(), Light())
+    logging.getLogger().info(f"Is message unread now? {message.is_unread()}")
     # run_loop(Button(), Light(), Message())
--- message/Message.py
+++ message/Message.py
@@ -1,17 +1,28 @@
+from pathlib import Path
+
+_MSG_FILE_NAME = 'message.txt'
+_UNREAD_FILE_NAME = 'unread.txt'
+
 
 class Message:
 
-    def __init__(self):
-        self._message = "msg"
-        self._unread = False
+    def __init__(self, root_path: Path):
+        if not (root_path.exists() and root_path.is_dir()):
+            root_path.mkdir()
+        self._msg_file = root_path / _MSG_FILE_NAME
+        if not self._msg_file.exists():
+            self._msg_file.touch()
+        self._unread_file = root_path / _UNREAD_FILE_NAME
+        if not self._unread_file.exists():
+            self._unread_file.touch()
 
     def is_unread(self):
-        return self._unread
+        return self._unread_file.read_text().strip() == 'True'
 
     def get_message(self):
-        self._unread = False
-        return self._message
+        self._unread_file.write_text('False')
+        return self._msg_file.read_text()
 
     def set_message(self, msg):
-        self._message = msg
-        self._unread = True
+        self._msg_file.write_text(msg)
+        self._unread_file.write_text('True')
